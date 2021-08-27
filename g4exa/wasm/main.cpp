1da29805d45cd9200e7762b2212a45e3f511b49d
Guy Barrand <guy.barrand@gmail.com>
2021-08-27 15:12:38 +0200

    update

---
+++ g4exa/wasm/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file g4exa.license for terms.
+
+#include "../g4exa/main"
+
+#define APP_NAME g4exa
+
+#include <exlib/app/wasm/main_cpp>
+
+//exlib_build_use inlib expat
+//exlib_build_use exlib png jpeg zlib inlib_glutess freetype kernel
+
+//exlib__build_use skip
